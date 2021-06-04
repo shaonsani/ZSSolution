d93ba1ddb067daf1ba8f6f532f1f0aafa4d8656e
shaonsani <shaon.sani.ss1@gmail.com>
2021-06-04 14:12:28 +0600

    Create a serializer for state list

--- country/serializers.py
+++ country/serializers.py
@@ -1,8 +1,16 @@
-from country.models import Country
+from country.models import Country, State
 from rest_framework import serializers
 
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
+        fields = '__all__'
+
+
+class StateSerializer(serializers.ModelSerializer):
+    country = serializers.ReadOnlyField(source='country.name')
+
+    class Meta:
+        model = State
         fields = '__all__'
