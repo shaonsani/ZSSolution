a44930b86a343a213a1a5a7bd11c382d51455a7e
shaonsani <shaon.sani.ss1@gmail.com>
2021-06-04 11:32:34 +0600

    Address model added into country app

--- country/admin.py
+++ country/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from country.models import Country, State
+from country.models import Country, State, Address
 
 admin.site.register(Country)
 admin.site.register(State)
+admin.site.register(Address)
--- country/models.py
+++ country/models.py
@@ -13,9 +13,23 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+
 class State(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='state')
+
+    class Meta:
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    house_number = models.CharField(max_length=255)
+    road_number = models.IntegerField()
+    state = models.ForeignKey(State, on_delete=models.CASCADE, related_name='address')
 
     class Meta:
         ordering = ('-id',)
