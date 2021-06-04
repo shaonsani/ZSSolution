461ff66f1492e151854cb67cce6e3a0f8c8dec0d
shaonsani <shaon.sani.ss1@gmail.com>
2021-06-04 10:57:21 +0600

    Country model added into country app

--- country/admin.py
+++ country/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from country.models import Country
 
-# Register your models here.
+admin.site.register(Country)
--- country/models.py
+++ country/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    code = models.CharField(max_length=3)
+
+    class Meta:
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.name
