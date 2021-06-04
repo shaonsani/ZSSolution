343ad6d8fc9f5331c7aef81c28c9724c56ef9be7
shaonsani <shaon.sani.ss1@gmail.com>
2021-06-04 14:29:04 +0600

    Create an api for address list with 2 optional filter

--- country/urls.py
+++ country/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from country.views import CountryAPI, StateAPI
+from country.views import CountryAPI, StateAPI, AddressAPI
 
 urlpatterns = [
     path('countries', CountryAPI.as_view(), name='country_list'),
     path('states', StateAPI.as_view(), name='state_list'),
+    path('address', AddressAPI.as_view(), name='address_list'),
 ]
--- country/views.py
+++ country/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from component.helper.response_helper import SuccessResponse, ErrorResponse
-from country.models import Country, State
-from country.serializers import CountrySerializer, StateSerializer
+from country.models import Country, State, Address
+from country.serializers import CountrySerializer, StateSerializer, AddressSerializer
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 
@@ -63,6 +63,41 @@ class StateAPI(APIView):
                 states = paginator.page(1)
 
             serializer = StateSerializer(states, many=True)
+            response = {
+                "data": serializer.data,
+                "total_data": paginator.count,
+                "current_page": page,
+                "last_page": paginator.num_pages
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = ErrorResponse("Page, Limit must be provided!")
+            return Response(response.to_json(), status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddressAPI(APIView):
+
+    def get(self, request):
+        page = request.query_params.get('page')
+        limit = request.query_params.get('limit')
+        house_number = request.query_params.get('house_number')
+        road_number = request.query_params.get('road_number')
+        q = Q()
+        if page and limit:
+            if house_number:
+                q &= Q(house_number=house_number)
+            if road_number:
+                q &= Q(road_number=road_number)
+            address_list = Address.objects.filter(q)
+            paginator = Paginator(address_list, limit)
+            try:
+                address_list = paginator.page(page)
+            except EmptyPage:
+                address_list = paginator.page(paginator.num_pages)
+            except PageNotAnInteger:
+                address_list = paginator.page(1)
+
+            serializer = AddressSerializer(address_list, many=True)
             response = {
                 "data": serializer.data,
                 "total_data": paginator.count,
