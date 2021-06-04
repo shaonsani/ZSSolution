bedd972e014efe4a86981454663ff9579c9bf272
shaonsani <shaon.sani.ss1@gmail.com>
2021-06-04 14:14:56 +0600

    Create an API for state list with name filter

--- country/urls.py
+++ country/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from country.views import CountryAPI
+from country.views import CountryAPI, StateAPI
 
 urlpatterns = [
     path('countries', CountryAPI.as_view(), name='country_list'),
+    path('states', StateAPI.as_view(), name='state_list'),
 ]
--- country/views.py
+++ country/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from component.helper.response_helper import SuccessResponse, ErrorResponse
-from country.models import Country
-from country.serializers import CountrySerializer
+from country.models import Country, State
+from country.serializers import CountrySerializer, StateSerializer
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 
@@ -31,6 +31,38 @@ class CountryAPI(APIView):
                 countries = paginator.page(1)
 
             serializer = CountrySerializer(countries, many=True)
+            response = {
+                "data": serializer.data,
+                "total_data": paginator.count,
+                "current_page": page,
+                "last_page": paginator.num_pages
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = ErrorResponse("Page, Limit must be provided!")
+            return Response(response.to_json(), status=status.HTTP_400_BAD_REQUEST)
+
+
+class StateAPI(APIView):
+
+    def get(self, request):
+        page = request.query_params.get('page')
+        limit = request.query_params.get('limit')
+        name = request.query_params.get('name')
+        q = Q()
+        if page and limit:
+            if name:
+                q &= Q(name=name)
+            states = State.objects.filter(q)
+            paginator = Paginator(states, limit)
+            try:
+                states = paginator.page(page)
+            except EmptyPage:
+                states = paginator.page(paginator.num_pages)
+            except PageNotAnInteger:
+                states = paginator.page(1)
+
+            serializer = StateSerializer(states, many=True)
             response = {
                 "data": serializer.data,
                 "total_data": paginator.count,
