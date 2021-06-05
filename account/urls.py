b4c460cd647317cb4c480bee2b4d24f431bb4edb
shaonsani <shaon.sani.ss1@gmail.com>
2021-06-05 23:52:40 +0600

    Create login api for authentication

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from account.views import UserRegister
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('register', UserRegister.as_view(), name='use_register'),
+    path('register', UserRegister.as_view(), name='user_register'),
+    path('login', obtain_auth_token, name='user_login'),
 ]
--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-9d6%r!%n)*+!(m6zp&+n-vv&#p_9x^653rl84a77f8#x=o=o!y
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -40,7 +38,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'account',
     'country',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -82,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -102,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -115,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
