54c6a2105b9f3deabcb88be20ff1203fb8996d3f
shaonsani <shaon.sani.ss1@gmail.com>
2021-06-05 13:11:43 +0600

    Add state list api testcase

--- country/tests.py
+++ country/tests.py
@@ -1,7 +1,7 @@
 import json
 from django.urls import reverse
-from country.models import Country
-from country.serializers import CountrySerializer
+from country.models import Country, State
+from country.serializers import CountrySerializer, StateSerializer
 from django.test import TestCase, Client
 from rest_framework import status
 
@@ -44,4 +44,45 @@ class CountryListTestCase(TestCase):
     def test_country_list_error_response(self):
         # get API response
         response = client.get(f'{self.country_list_url}')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class StateListTestCase(TestCase):
+    state_list_url = reverse("state_list")
+
+    def setUp(self):
+        self.india = Country.objects.create(name='India', latitude=37.45, longitude=14.45, code='IND')
+        self.bhutan = Country.objects.create(name='Bhutan', latitude=37.45, longitude=14.45, code='BHT')
+        State.objects.create(name='Mumbai', country=self.india)
+        State.objects.create(name='Thimpu', country=self.bhutan)
+
+    def test_state_list(self):
+        # get API response
+        response = client.get(f'{self.state_list_url}?page=1&limit=10')
+        # get data from db
+        state_list = State.objects.all()
+        serializer = StateSerializer(state_list, many=True)
+        self.assertEqual(response.data['data'], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_state_list_with_filter(self):
+        # get API response
+        response = client.get(f'{self.state_list_url}?page=1&limit=10&name=Mumbai')
+        # get data from db
+        state_list = State.objects.filter(name='Mumbai')
+        serializer = StateSerializer(state_list, many=True)
+        self.assertEqual(response.data['data'], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_state_list_with_blank_filter(self):
+        # get API response
+        response = client.get(f'{self.state_list_url}?page=1&limit=10&name=')
+        # get data from db
+        state_list = State.objects.all()
+        serializer = StateSerializer(state_list, many=True)
+        self.assertEqual(response.data['data'], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_state_list_error_response(self):
+        # get API response
+        response = client.get(f'{self.state_list_url}')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
